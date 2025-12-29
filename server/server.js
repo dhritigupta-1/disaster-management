491380537b74e235e3af876c1ff184d4b093f77a
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-12-29 13:05:52 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -128,5 +128,5 @@ app.use("/api/admin/notes", protect, protectAdmin, incidentNoteRoutes);
 app.use("/api/sos", sosRoutes); // Public SOS
 
 // Run server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Backend running on port ${PORT}`));
