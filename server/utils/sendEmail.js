719ab78efeb03dd076dfdc67c44b0327a97f9010
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-28 16:32:02 +0530

    first commit

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -140,6 +140,9 @@ const sendEmail = async (options) => {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    tls: {
+    rejectUnauthorized: false, // 🔥 IMPORTANT on Render
+    },
     connectionTimeout: 20000,
     greetingTimeout: 20000,
     socketTimeout: 20000,
